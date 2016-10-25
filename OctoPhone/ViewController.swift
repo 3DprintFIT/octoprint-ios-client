3db7582a34500415ca190b67309bba2768e11859
Josef Dolezal <pepik.ml@gmail.com>
2016-10-25 09:55:52 +0200

    CODE: Create base project

---
+++ OctoPhone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OctoPhone
+//
+//  Created by Josef Dolezal on 25/10/16.
+//  Copyright © 2016 Josef Dolezal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
