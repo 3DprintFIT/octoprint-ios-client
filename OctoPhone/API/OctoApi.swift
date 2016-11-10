3a5349a4ba49d350ef4f7b6bbe5675aa8f2afc98
Josef Dolezal <pepik.ml@gmail.com>
2016-11-10 12:36:38 +0100

    LIB: Import compiled libs to project

---
+++ OctoPhone/API/OctoApi.swift
@@ -0,0 +1,10 @@
+//
+//  OctoApi.swift
+//  OctoPhone
+//
+//  Created by Josef Dolezal on 10/11/16.
+//  Copyright © 2016 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+import Alamofire
