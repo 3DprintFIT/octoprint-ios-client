e7e3667923843f763d06197954b59d8fb8c20374
Josef Dolezal <pepik.ml@gmail.com>
2016-12-04 01:10:16 +0100

    FEAT: WIP Create printer list collection view

---
+++ OctoPhone/View Related/Cells/PrinterOverviewCollectionViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  PrinterOverviewCollectionViewCell.swift
+//  OctoPhone
+//
+//  Created by Josef Dolezal on 04/12/2016.
+//  Copyright © 2016 Josef Dolezal. All rights reserved.
+//
+
+import UIKit
+
+/// Real word printer overview
+class PrinterOverviewCollectionViewCell: UICollectionViewCell {
+
+    /// CollectionView reuse identifier
+    static let identifier = "PrinterOverviewCollectionViewCell"
+
+    /// Printer photo
+    let photo = UIImageView()
+
+    /// Printing status label
+    let statusLabel = UILabel()
+
+    /// Printing progress view
+    let printProgress = UIProgressView()
+
+    /// Holds all view size constants
+    struct Sizes {
+        /// Height of the printer photo view
+        static let photoHeight: CGFloat = 120
+
+        /// Default left margin
+        static let leftMargin: CGFloat = 8
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configureView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureView() {
+        let subviews = [photo, statusLabel, printProgress]
+
+        subviews.forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(view)
+        }
+
+        let constraints = [
+            photo.topAnchor.constraint(equalTo: topAnchor),
+            photo.leadingAnchor.constraint(equalTo: leadingAnchor),
+            photo.widthAnchor.constraint(equalTo: widthAnchor),
+            photo.heightAnchor.constraint(equalToConstant: Sizes.photoHeight),
+            statusLabel.topAnchor.constraint(equalTo: photo.bottomAnchor),
+            statusLabel.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: Sizes.leftMargin
+            )
+        ]
+
+        constraints.forEach { $0.isActive = true }
+    }
+}
---
+++ OctoPhone/View Related/Printer List/PrinterListCollectionViewController.swift
@@ -0,0 +1,77 @@
+//
+//  PrinterListCollectionViewController.swift
+//  OctoPhone
+//
+//  Created by Josef Dolezal on 01/12/2016.
+//  Copyright © 2016 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Represents list of users printers
+class PrinterListCollcetionViewController: UICollectionViewController {
+    /// Data context manager
+    private let contextManager: ContextManager
+
+    /// New Printer list controller instance
+    /// operating on given context
+    ///
+    /// - Parameter contextManager: Data context manager
+    init(contextManager: ContextManager) {
+        self.contextManager = contextManager
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    /// Required initializer
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureView()
+    }
+
+    /// Configures all view-related properties
+    private func configureView() {
+        collectionView?.register(
+            PrinterOverviewCollectionViewCell.self,
+            forCellWithReuseIdentifier: PrinterOverviewCollectionViewCell.identifier
+        )
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension PrinterListCollcetionViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2 // Local printers and user printers
+    }
+
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 5
+    }
+
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+        ) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: PrinterOverviewCollectionViewCell.identifier,
+            for: indexPath
+        )
+
+        return cell
+    }
+}
+
+//MARK: - UICollectionViewDelegate
+extension PrinterListCollcetionViewController {
+    
+}
