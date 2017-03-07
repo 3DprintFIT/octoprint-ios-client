9aa67cc69796e2210df03fed302a40ae13d79f0b
Josef Dolezal <pepik.ml@gmail.com>
2017-03-07 12:37:39 +0100

    TESTS: Add tests for Printer List Cell view model

---
+++ OctoPhoneTests/VIew Related/Printer List/PrinterListCellViewModelTests.swift
@@ -0,0 +1,26 @@
+//
+//  PrinterListCellViewModelTests.swift
+//  OctoPhone
+//
+//  Created by Josef Dolezal on 07/03/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Nimble
+import Quick
+@testable import OctoPhone
+
+class PrinterListCellViewModelTests: QuickSpec {
+    override func spec() {
+        it("should map model correctly") {
+            let printerPath = "http://localhost"
+            let printerName = "My Printer"
+
+            let printer = Printer(url: URL(string: printerPath)!, accessToken: "Secret Token", name: printerName)
+            let subject = PrinterListCellViewModel(printer: printer)
+
+            expect(subject.printerName) == printerName
+            expect(subject.printerURL) == printerPath
+        }
+    }
+}
