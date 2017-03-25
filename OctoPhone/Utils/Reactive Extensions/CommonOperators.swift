85ce1a3f961d1f0b2687a39e6d61c26d0e2f2e9b
Josef Dolezal <pepik.ml@gmail.com>
2017-03-25 17:28:13 +0100

    FEAT: Add ignoreValues operator

--- OctoPhone/Utils/Reactive Extensions/CommonOperators.swift
+++ OctoPhone/Utils/Reactive Extensions/CommonOperators.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2017 Josef Dolezal. All rights reserved.
 //
 
-import Foundation
+import ReactiveSwift
+import Result
+
+/// Add skipError operator for producers which may produce error
+extension SignalProducerProtocol where Error == Error {
+    /// Add ability to skip errors produced by `self`
+    ///
+    /// - Returns: Producer, which sends empty producer on error
+    func skipError() -> SignalProducer<Value, NoError> {
+        return flatMapError { _ in return SignalProducer<Self.Value, NoError>.empty }
+    }
+}
+
+extension SignalProducerProtocol {
+    /// Map each value of `self` to Void
+    ///
+    /// - Returns: New producer, where original values are replaced by Void
+    func ignoreValues() -> SignalProducer<(), Self.Error> {
+        return map({ _ in })
+    }
+}
+
+extension SignalProtocol {
+    /// Map each value to Void
+    ///
+    /// - Returns: New signal, where original values are replaced by Void
+    func ignoreValues() -> Signal<(), Self.Error> {
+        return map({ _ in return })
+    }
+}
