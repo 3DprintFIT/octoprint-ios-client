382faaa5036c55c829159fb3d38005a9465b37d2
Josef Dolezal <pepik.ml@gmail.com>
2017-04-05 23:40:04 +0200

    WIP: Create form text input view

--- OctoPhone/View Related/Print Profile/FormTextInputView.swift
+++ OctoPhone/View Related/Print Profile/FormTextInputView.swift
@@ -11,9 +11,23 @@ import SnapKit
 
 class FormTextInputView: UIView {
 
+    struct Layout {
+        static let sideMargin: CGFloat = 15
+        static let padding: CGFloat = 7
+        static let minTextFieldSize = 28
+    }
+
     weak var descriptionLabel: UILabel!
 
     weak var textField: UITextField!
+
+    weak var suffixLabel: UILabel!
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 100,
+                      height: Layout.padding + descriptionLabel.frame.height + Layout.padding
+                        + textField.frame.height + Layout.padding)
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,20 +36,36 @@ class FormTextInputView: UIView {
 
         let descriptionLabel = UILabel()
         let textField = UITextField()
-
-        descriptionLabel.text = "Name"
-        descriptionLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
-        descriptionLabel.snp.makeConstraints { make in
-            make.leading.trailing.top.equalToSuperview().inset(UIEdgeInsets(top: 7, left: 15, bottom: 0, right: 15))
-        }
-
-        textField.snp.makeConstraints { make in
-            make.top.equalTo(descriptionLabel.snp.bottom)
-            make.leading.trailing.equalToSuperview()
-        }
+        let suffixLabel = UILabel()
 
         addSubview(descriptionLabel)
         addSubview(textField)
+        addSubview(suffixLabel)
+
+        descriptionLabel.text = "Name"
+        descriptionLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        suffixLabel.text = "mm"
+        descriptionLabel.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview().inset(
+                UIEdgeInsets(top: Layout.padding, left: Layout.sideMargin, bottom: 0,
+                             right: Layout.sideMargin))
+        }
+
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(Layout.padding)
+            make.leading.equalToSuperview().offset(Layout.padding)
+            make.trailing.equalTo(suffixLabel.snp.leading).inset(Layout.padding)
+            make.height.greaterThanOrEqualTo(Layout.minTextFieldSize)
+            make.bottom.equalToSuperview().inset(Layout.padding)
+        }
+
+        suffixLabel.snp.makeConstraints { make in
+            make.lastBaseline.equalTo(textField)
+            make.trailing.equalToSuperview().inset(Layout.sideMargin)
+        }
+
+        self.descriptionLabel = descriptionLabel
+        self.textField = textField
     }
 
     required init?(coder aDecoder: NSCoder) {
