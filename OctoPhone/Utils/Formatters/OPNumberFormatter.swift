e1f976ea92ac03782be2527f5b065d361a15e73e
Josef Dolezal <pepik.ml@gmail.com>
2017-04-10 16:16:18 +0200

    FEAT: Add number formatters for length and volume

--- OctoPhone/Utils/Formatters/OPNumberFormatter.swift
+++ OctoPhone/Utils/Formatters/OPNumberFormatter.swift
@@ -19,9 +19,19 @@ final class OPNumberFormatter {
         let decimalFormatter = NumberFormatter()
 
         decimalFormatter.numberStyle = .decimal
-        decimalFormatter.groupingSeparator = " "
+        decimalFormatter.locale = Locale.current
 
         return decimalFormatter
+    }()
+
+    /// Shared instance for measurements formatting
+    private let measurementFormatter: MeasurementFormatter = {
+        let formatter = MeasurementFormatter()
+
+        formatter.locale = Locale.current
+        formatter.unitOptions = .naturalScale
+
+        return formatter
     }()
 
     private init() { }
@@ -40,5 +50,25 @@ final class OPNumberFormatter {
     /// - Returns: String represetation of given number with standard format
     func plainNumber(_ number: Double) -> String {
         return standardNumberFormatter.string(from: NSNumber(value: number))!
+    }
+
+    /// Formats given amount to length string
+    ///
+    /// - Parameters:
+    ///   - amount: Actual amount of unformatted length
+    ///   - unit: Unit of given amount. Default `millimeter`
+    /// - Returns: Formatted length string
+    func length(_ amount: Double, fromUnit unit: UnitLength = .millimeters) -> String {
+        return measurementFormatter.string(from: Measurement(value: amount, unit: unit))
+    }
+
+    /// Formats given amount to volume string
+    ///
+    /// - Parameters:
+    ///   - amount: Actual amount of unformatted volume
+    ///   - unit: Unit of given amount. Default `squareMillimeters`
+    /// - Returns: Formatted volume string
+    func volume(_ amount: Double, unit: UnitArea = .squareMillimeters) -> String {
+        return measurementFormatter.string(from: Measurement(value: amount, unit: unit))
     }
 }
