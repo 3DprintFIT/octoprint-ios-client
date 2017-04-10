54efee73bd2eb12ed43825a5fbc4289a7171880d
Josef Dolezal <pepik.ml@gmail.com>
2017-04-10 12:00:50 +0200

    FEAT: Create standard date formatter

--- OctoPhone/Utils/Formatters/OPDateFormatter.swift
+++ OctoPhone/Utils/Formatters/OPDateFormatter.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+/// Provides standardized date formats
+struct OPDateFormatter {
+    /// Creates date from given timestamp
+    ///
+    /// - Parameter timestamp: UNIX timestamp
+    /// - Returns: Formatted date representing given timestamp
+    static func dateFromTimeStamp(_ timestamp: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale.current
+
+        return dateFormatter.string(from: date)
+    }
+}
