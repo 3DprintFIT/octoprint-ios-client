766c7245b99a30ebcea401c3c46cb51fcf689024
Josef Dolezal <pepik.ml@gmail.com>
2017-04-18 11:41:43 +0200

    FEAT: Add support for gcode and stl file importing

---
+++ OctoPhone/Utils/Constants/UTI.swift
@@ -0,0 +1,18 @@
+//
+//  UTI.swift
+//  OctoPhone
+//
+//  Created by Josef Dolezal on 18/04/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+/// Namespace for supported UTI file identifiers
+struct UTI {
+    /// UTI for GCode files
+    static let gcode = "cz.ajty.OctoPhone.UTI.gcode"
+
+    /// UTI for stl files
+    static let stl = "public.standard-tesselated-geometry-format"
+}
