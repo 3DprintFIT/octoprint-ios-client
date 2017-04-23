b0ecb3162971a5cc4f1f4357e19a6cca0c234f95
Josef Dolezal <pepik.ml@gmail.com>
2017-04-23 18:13:48 +0200

    REF: Make SD card management VC and VM consistent

--- OctoPhone/View Related/SD Card Management/SDCardManagementViewController.swift
+++ OctoPhone/View Related/SD Card Management/SDCardManagementViewController.swift
@@ -2,25 +2,41 @@
 //  SDCardManagementViewController.swift
 //  OctoPhone
 //
-//  Created by Josef Dolezal on 09/03/2017.
+//  Created by Josef Dolezal on 23/04/2017.
 //  Copyright © 2017 Josef Dolezal. All rights reserved.
 //
 
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
 
-/// Controller for SD Card operations and management
+/// Controller for SD card management
 class SDCardManagementViewController: BaseViewController {
+    // MARK: - Properties
 
-    /// Card management logic
-    private var viewModel: SDCardManagementViewModelType!
+    /// Controller logic
+    fileprivate var viewModel: SDCardManagementViewModelType!
+
+    // MARK: - Initializers
 
     convenience init(viewModel: SDCardManagementViewModelType) {
         self.init()
 
         self.viewModel = viewModel
+        bindViewModel()
     }
+
+    // MARK: - Controller lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // MARK: - Internal logic
+
+    /// Binds outputs of View Model to UI and converts
+    /// user interaction to View Model inputs
+    private func bindViewModel() {
+
     }
 }
--- OctoPhone/View Related/SD Card Management/SDCardManagementViewModel.swift
+++ OctoPhone/View Related/SD Card Management/SDCardManagementViewModel.swift
@@ -2,36 +2,54 @@
 //  SDCardManagementViewModel.swift
 //  OctoPhone
 //
-//  Created by Josef Dolezal on 09/03/2017.
+//  Created by Josef Dolezal on 23/04/2017.
 //  Copyright © 2017 Josef Dolezal. All rights reserved.
 //
 
-import Foundation
+import ReactiveSwift
+import Result
 
+// MARK: - Inputs
 /// SD card management inputs
 protocol SDCardManagementViewModelInputs {
 
 }
 
+// MARK: - Outputs
 /// SD card management outputs
 protocol SDCardManagementViewModelOutputs {
 
 }
 
+// MARK: - Common public interface
 /// Common interface for SD card managing view models
 protocol SDCardManagementViewModelType {
-    /// Avaialable SD card management inputs
+    /// Available inputs
     var inputs: SDCardManagementViewModelInputs { get }
 
-    /// Avaialable SD card management outputs
+    /// Available outputs
     var outputs: SDCardManagementViewModelOutputs { get }
 }
 
+// MARK: - View Model implementation
 /// SD Card management logic
-final class SDCardManagementViewModel: SDCardManagementViewModelType,
-SDCardManagementViewModelInputs, SDCardManagementViewModelOutputs {
-
+final class SDCardManagementViewModel: SDCardManagementViewModelType, SDCardManagementViewModelInputs,
+SDCardManagementViewModelOutputs {
     var inputs: SDCardManagementViewModelInputs { return self }
 
     var outputs: SDCardManagementViewModelOutputs { return self }
+
+    // MARK: Inputs
+
+    // MARK: Outputs
+
+    // MARK: Private properties
+
+    // MARK: Initializers
+
+    // MARK: Input methods
+
+    // MARK: Output methods
+
+    // MARK: Internal logic
 }
