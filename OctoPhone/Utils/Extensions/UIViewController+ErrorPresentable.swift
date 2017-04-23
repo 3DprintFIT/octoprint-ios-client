d1f9d2fc7990f78857243869b6d8b02a3d82ea83
Josef Dolezal <pepik.ml@gmail.com>
2017-04-23 11:41:40 +0200

    FEAT: Add binding target for errors to controllers

--- OctoPhone/Utils/Extensions/UIViewController+ErrorPresentable.swift
+++ OctoPhone/Utils/Extensions/UIViewController+ErrorPresentable.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
+
+/// Represents error, which can be presented to user with alert
+typealias DisplayableError = (title: String?, message: String?)
 
 protocol ErrorPresentable {
     /// Presents error with given title and message,
@@ -26,5 +31,19 @@ extension ErrorPresentable where Self: UIViewController {
         controller.addAction(okAction)
 
         present(controller, animated: true, completion: nil)
+    }
+}
+
+extension Reactive where Base: ErrorPresentable, Base: UIViewController {
+    /// Creates new binding target for controllers which are able to display error,
+    /// error is presented on main thread and target uses lifetime of its base.
+    var displayableError: BindingTarget<DisplayableError> {
+        let lifetime = base.reactive.lifetime
+
+        return BindingTarget(on: UIScheduler(), lifetime: lifetime) { [weak base = self.base] error in
+            if let base = base {
+                base.presentError(title: error.title, message: error.message)
+            }
+        }
     }
 }
