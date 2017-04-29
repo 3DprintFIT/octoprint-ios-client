640dc57d2c438c7f0a00fb8c082d7acac7cd33c1
Josef Dolezal <pepik.ml@gmail.com>
2017-04-29 12:44:41 +0200

    UI: Finish layout for controls view

--- OctoPhone/View Related/Controls/ControlsView.swift
+++ OctoPhone/View Related/Controls/ControlsView.swift
@@ -30,28 +30,38 @@ class ControlsView: UIView {
     /// Moves head in negative direction on Y axis
     let moveBackButton = ControlsView.moveButton()
 
+    /// Move the print head home on X and Y axis
+    let moveHomeXYButton = ControlsView.homeButton()
+
     /// Moves head in positive direction on Z axis
     let moveUpButton = ControlsView.moveButton()
 
     /// Moves head in negative direction on Z axis
     let moveDownButton = ControlsView.moveButton()
 
+    /// Moves the print head home on Z axis
+    let moveHomeZButton = ControlsView.homeButton()
+
     // MARK: Private properties
 
     /// Holds all views on rounded click wheel
     private let clickWheelView = UIView()
 
+    /// Stack for vertically alligned click wheel buttons
     private let clickStackView: UIStackView
 
     /// Holds all controls operating on Z axis
-    private let roundedControlView: UIStackView
+    private let roundedControlView = UIView()
+
+    /// Stack for vertically aligned rounded controls
+    private let roundedStackView: UIStackView
 
     // MARK: Initializers
 
     init() {
-        self.roundedControlView = UIStackView(arrangedSubviews: [moveUpButton, moveDownButton],
-                                              axis: .vertical)
-        self.clickStackView = UIStackView(arrangedSubviews: [moveForwardButton, moveBackButton],
+        self.roundedStackView = UIStackView(arrangedSubviews: [moveUpButton, moveHomeZButton, moveDownButton],
+                                            axis: .vertical)
+        self.clickStackView = UIStackView(arrangedSubviews: [moveForwardButton, moveHomeXYButton, moveBackButton],
                                           axis: .vertical)
 
         super.init(frame: .zero)
@@ -62,8 +72,9 @@ class ControlsView: UIView {
         clickWheelView.addSubview(clickStackView)
         clickWheelView.addSubview(moveLeftButton)
         clickWheelView.addSubview(moveRightButton)
+        roundedControlView.addSubview(roundedStackView)
 
-        roundedControlView.distribution = .equalSpacing
+        roundedStackView.distribution = .equalSpacing
         clickStackView.distribution = .equalSpacing
     }
 
@@ -85,12 +96,37 @@ class ControlsView: UIView {
         roundedControlView.snp.makeConstraints { make in
             make.trailing.equalToSuperview().inset(50)
             make.height.centerY.equalTo(clickWheelView)
-            make.leadingMargin.trailingMargin.equalTo(15)
+            make.width.equalTo(90)
         }
 
-        clickWheelView.backgroundColor = Colors.Views.defaultControllerBackground
+        moveLeftButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(15)
+        }
+
+        moveRightButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(15)
+        }
+
+        clickStackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalToSuperview().inset(15)
+        }
+
+        roundedStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(15)
+        }
+
+        roundedStackView.isLayoutMarginsRelativeArrangement = true
+        roundedControlView.layoutMargins = .init(top: 0, left: 15, bottom: 0, right: 15)
         roundedControlView.backgroundColor = Colors.Views.defaultControllerBackground
-        roundedControlView.isLayoutMarginsRelativeArrangement = true
+        roundedControlView.layer.cornerRadius = 15
+        roundedControlView.layer.masksToBounds = true
+
+        clickWheelView.layer.cornerRadius = 125
+        clickWheelView.layer.masksToBounds = true
+        clickWheelView.backgroundColor = Colors.Views.defaultControllerBackground
     }
 
     // MARK: Internal logic
@@ -101,6 +137,16 @@ class ControlsView: UIView {
         button.snp.makeConstraints { make in
             make.width.height.equalTo(60)
         }
+
+        return button
+    }
+
+    private static func homeButton() -> UIButton {
+        let button = moveButton()
+
+        button.layer.cornerRadius = 30
+        button.layer.masksToBounds = true
+        button.backgroundColor = .white
 
         return button
     }
