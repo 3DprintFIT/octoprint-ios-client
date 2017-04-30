4d1282c32bced4fd03557423756dcff24b88d813
Josef Dolezal <pepik.ml@gmail.com>
2017-04-30 22:32:11 +0200

    UI: Add FontAwesome icon font

---
+++ Icons/Icons.h
@@ -0,0 +1,19 @@
+//
+//  Icons.h
+//  Icons
+//
+//  Created by Josef Dolezal on 29/04/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Icons.
+FOUNDATION_EXPORT double IconsVersionNumber;
+
+//! Project version string for Icons.
+FOUNDATION_EXPORT const unsigned char IconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Icons/PublicHeader.h>
+
+
